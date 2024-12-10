ef793b08e491778562c68f2b891e22802e88f810
Yaser_arabath <159247522+yaser-hash@users.noreply.github.com>
2024-12-10 09:43:09 +0530

    Add files via upload

---
+++ scott.sql
@@ -0,0 +1,107 @@
+create database scott;
+use scott;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bonus`
+--
+
+CREATE TABLE  `bonus` (
+  `ENAME` varchar(10) DEFAULT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `SAL` int(11) DEFAULT NULL,
+  `COMM` int(11) DEFAULT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dept`
+--
+
+CREATE TABLE  `dept` (
+  `DEPTNO` int(11) DEFAULT NULL,
+  `DNAME` varchar(14) DEFAULT NULL,
+  `LOC` varchar(13) DEFAULT NULL
+);
+
+--
+-- Dumping data for table `dept`
+--
+
+INSERT INTO `dept` (`DEPTNO`, `DNAME`, `LOC`) VALUES
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dummy`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp`
+--
+
+CREATE TABLE  `emp` (
+  `EMPNO` int(11) NOT NULL,
+  `ENAME` varchar(10) DEFAULT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `MGR` int(11) DEFAULT NULL,
+  `HIREDATE` date DEFAULT NULL,
+  `SAL` int(11) DEFAULT NULL,
+  `COMM` int(11) DEFAULT NULL,
+  `DEPTNO` int(11) DEFAULT NULL
+) ;
+
+--
+-- Dumping data for table `emp`
+--
+
+INSERT INTO `emp` (`EMPNO`, `ENAME`, `JOB`, `MGR`, `HIREDATE`, `SAL`, `COMM`, `DEPTNO`) VALUES
+(7369, 'SMITH', 'CLERK', 7902, '1980-12-17', 800, NULL, 20),
+(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600, 300, 30),
+(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250, 500, 30),
+(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975, NULL, 20),
+(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30),
+(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850, NULL, 30),
+(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450, NULL, 10),
+(7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', 3000, NULL, 20),
+(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000, NULL, 10),
+(7844, 'TURNER', 'SALESMAN', 7698, '1980-09-08', 1500, 0, 30),
+(7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', 1100, NULL, 20),
+(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950, NULL, 30),
+(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000, NULL, 20),
+(7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300, NULL, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salgrade`
+--
+
+CREATE TABLE  `salgrade` (
+  `GRADE` int(11) DEFAULT NULL,
+  `LOSAL` int(11) DEFAULT NULL,
+  `HISAL` int(11) DEFAULT NULL
+) ;
+
+--
+-- Dumping data for table `salgrade`
+--
+
+INSERT INTO `salgrade` (`GRADE`, `LOSAL`, `HISAL`) VALUES
+(1, 700, 1200),
+(2, 1201, 1400),
+(3, 1401, 2000),
+(4, 2001, 3000),
+(5, 3001, 9999);
+
+show tables;
+
+select * from emp;
